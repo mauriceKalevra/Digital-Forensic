621e336a809fda52773b2fa82bc846e4e66608d7
Maurice <Mori.r@web.de>
2022-07-05 16:21:44 +0200

    build Github Skyline

--- buildSkyscraper.sh
+++ buildSkyscraper.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-echo "Eingabe höhe Github Skyline: "
-read height
+function randomChange() {
 
-l=15
-[ -n "$1" ] && l=$1
-[ -n "$2" ] && l=$(shuf --random-source=/dev/urandom -i $1-$2 -n 1)
-tr -dc A-Za-z0-9 < /dev/urandom | head -c ${l} | xargs>skyline.txt
+	
+	echo "Eingabe höhe Github Skyline: "
+	read height
 
-git add .
-git commit -m "build Github Skyline"
-git push
+	l=15
+	[ -n "$1" ] && l=$1
+	[ -n "$2" ] && l=$(shuf --random-source=/dev/urandom -i $1-$2 -n 1)
+	tr -dc A-Za-z0-9 < /dev/urandom | head -c ${l} | xargs>skyline.txt
+
+	git add .
+	git commit -m "build Github Skyline"
+	git push
+}
+
+randomChange
